139fd855e0e89d0ddae9dc69b3ec72c8c2bde399
sava <savamedvedev@gmail.com>
2022-04-25 19:25:50 +0800

    fiver-commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,71 @@
 import React from "react";
 import "./FooterStyles.css";
+import { SiDatabricks } from "react-icons/si";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
+import {
+  FiMail,
+  FiFacebook,
+  FiGithub,
+  FiInstagram,
+  FiLinkedin,
+  FiDribbble,
+} from "react-icons/fi";
+
 const Footer = () => {
   return (
     <div className="footer">
       <div className="container">
-        <div className="content">
-          <ul>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-          </ul>
+        <div className="top">
+          <div className="logo-footer">
+            <SiDatabricks className="icon" />
+            <h2>Secured.</h2>
+          </div>
+          <BsFillArrowUpCircleFill className="icon" />
+        </div>
+        <div className="col-container">
+          <div className="col">
+            <h3>Navigation</h3>
+            <p>Home</p>
+            <p>Data</p>
+            <p>Cloud</p>
+            <p>Contact</p>
+          </div>
+
+          <div className="col">
+            <h3>My Account</h3>
+            <p>Home</p>
+            <p>Data</p>
+            <p>Cloud</p>
+            <p>Contact</p>
+          </div>
+
+          <div className="col">
+            <h3>Information</h3>
+            <p>Home</p>
+            <p>Data</p>
+            <p>Cloud</p>
+            <p>Contact</p>
+          </div>
+
+          <div className="col">
+            <h3>Legal</h3>
+            <p>Home</p>
+            <p>Data</p>
+            <p>Cloud</p>
+            <p>Contact</p>
+          </div>
+          <form>
+            <h3>Join Our Team</h3>
+            <input type="email" placeholder="Enter your email address" />
+            <FiMail className="mail-icon" />
+            <div className="social-group">
+              <FiInstagram className="social-icon" />
+              <FiFacebook className="social-icon" />
+              <FiLinkedin className="social-icon" />
+              <FiDribbble className="social-icon" />
+              <FiGithub className="social-icon" />
+            </div>
+          </form>
         </div>
       </div>
     </div>
