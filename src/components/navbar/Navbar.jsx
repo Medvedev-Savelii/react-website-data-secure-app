3629cf73afe3c9b417330212b4b9b2207117e9a2
sava <savamedvedev@gmail.com>
2022-04-25 23:00:52 +0800

    eight-commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./NavbarStyles.css";
 import { SiDatabricks } from "react-icons/si";
 import { FaBars, FaTimes } from "react-icons/fa";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleNav = () => setNav(!nav);
@@ -15,10 +15,18 @@ const Navbar = () => {
           <h1>Secured</h1>
         </div>
         <ul className={nav ? " nav-menu active" : "nav-menu"}>
-          <li>Home</li>
-          <li>Recovery</li>
-          <li>Cloud</li>
-          <li>Contact</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/recovery">Recovery</Link>
+          </li>
+          <li>
+            <Link to="/cloud">Cloud</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
           <button>Sign in</button>
         </ul>
         <div className="hamburger" onClick={handleNav}>
