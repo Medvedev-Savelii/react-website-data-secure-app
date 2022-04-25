7804667b71c85e3af6c53f6aa49b279ed254f1a6
sava <savamedvedev@gmail.com>
2022-04-25 22:20:27 +0800

    seven-commit

--- src/components/recovery/Recovery.jsx
+++ src/components/recovery/Recovery.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import './RecoveryStyles.css';
+import React from "react";
+import "./RecoveryStyles.css";
 const Recovery = () => {
   return (
-	<div>Recovery</div>
-  )
-}
+    <div className="recovery">
+      <div className="container">
+        <div className="content">
+          <h2>
+            <span>Data</span> Recovery
+          </h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor ea,
+            ex, illo molestias totam accusamus minus ullam, facere autem impedit
+            mollitia sunt nulla asperiores! Hic minima quod velit deleniti
+            dolores!
+          </p>
+          <div>
+            <button>Learn More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Recovery
+export default Recovery;
